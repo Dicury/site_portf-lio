2ecebacdb630ae836c3883a8804fd584dbbc1829
Mateus Cury <mateus.dicury@gmail.com>
2023-11-29 14:04:23 -0300

    feat: links de contato adicionados

--- src/app/components/Footer/index.jsx
+++ src/app/components/Footer/index.jsx
@@ -10,16 +10,22 @@ import {
 function Footer() {
   return (
     <Container id="contato">
-      <IconeLink href="/">
+      <IconeLink
+        href="https://www.linkedin.com/in/mateus-cury-398962161/"
+        target="blank"
+      >
         <Icone src="/icones/Linkedin.svg" />
       </IconeLink>
-      <IconeLink href="/">
+      <IconeLink href="https://github.com/Dicury" target="blank">
         <Icone src="/icones/GitHub.svg" />
       </IconeLink>
-      <IconeLink href="/">
+      <IconeLink href="https://wa.me/5519998414801" target="blank">
         <Icone src="/icones/WhatsApp.svg" />
       </IconeLink>
-      <IconeLink href="/">
+      <IconeLink
+        href="mailto:mateus.dicury@gmail.com?subject=&body="
+        target="blank"
+      >
         <Icone src="/icones/Email.svg" />
       </IconeLink>
     </Container>
